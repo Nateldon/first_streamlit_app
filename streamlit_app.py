544bf03f86565a85f940934c80460f9d8b5c67f8
MellyMel Melnyk <104394297+Nateldon@users.noreply.github.com>
2022-04-27 10:04:39 -0500

    Update streamlit_app.py
    Moved smoothie header because it was in the wrong place

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.header('🍌🍓 Build Your Own Fruit Smoothie🥝🍇') 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🍓 Build Your Own Fruit Smoothie🥝🍇') 
