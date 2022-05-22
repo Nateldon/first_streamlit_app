cf877540d29d2d272cd375f85804aafa7b3d17f6
MellyMel Melnyk <104394297+Nateldon@users.noreply.github.com>
2022-05-22 10:47:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write ('The user entered ', fruit_choice)
 
+
                  
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -76,4 +77,17 @@ streamlit.write ('Thanks for adding', add_my_fruit)
 # This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
+#new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+  else
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+  streamlit.dataframe(fruityvice_normalized)
+  
+  except URLError as e:
+    streamlit.error()
 
