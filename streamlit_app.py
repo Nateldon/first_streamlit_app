df03d2f4b06f591623e44075daa8675575cbe610
MellyMel Melnyk <104394297+Nateldon@users.noreply.github.com>
2022-05-22 12:36:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,32 +66,32 @@ try:
 
 #inport snowflake connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute ("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text ("Hello from Snowflake:")
-streamlit.text (my_data_row)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute ("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text ("Hello from Snowflake:")
+#streamlit.text (my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute ("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text ("The fruit load list contains:")
-streamlit.text (my_data_row)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute ("select * from fruit_load_list")
+#my_data_row = my_cur.fetchone()
+#streamlit.text ("The fruit load list contains:")
+#streamlit.text (my_data_row)
 
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#my_data_row = my_cur.fetchone()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_row)
 
-my_cur.execute ("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cur.execute ("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write ('Thanks for adding', add_my_fruit)
+#add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+#streamlit.write ('Thanks for adding', add_my_fruit)
 
 
 # This will not work correctly, but just go with it for now
